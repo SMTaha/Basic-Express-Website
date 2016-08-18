f06f421ae494cb680ad9d4c2ecbc4a372bdb349f
Syed M. Taha <s.m.taha10@gmail.com>
2016-08-18 21:38:12 +0500

    Minor updates

--- routes/contact.js
+++ routes/contact.js
@@ -10,27 +10,30 @@ router.post('/send', function (req, res, next) {
   var transporter = nodemailer.createTransport({
     service: 'Gmail',
     auth:{
-      user: 's.m.taha10@gmail.com',
-      pass: '456gmail852'
+      user: 'john@doe.com',         //Enter your email
+      pass: 'Enter your password'   //Enter your password
     }
   });
   var mailOptions = {
     form: 'John Doe <john@doe.com>',
-    to: 's.m.taha10@gmail.com',
+    to: 'john@doe.com',             //Enter your email
     subject: 'Website Submission',
     text: `You have a new submission with the following details ... \n
-           Name: {$req.body.name} \n
-           Email: {req.body.email} \n
-           Message: {req.body.message}
+           Name: ${req.body.name} \n
+           Email: ${req.body.email} \n
+           Password: ${req.body.password} \n
+           Message: ${req.body.message}
            `,
    html: `<p>You got a new submission with the following details...</p>
           <ul>
-            <li>Name: {req.body.name}</li>
-            <li>Email: {req.body.email}</li>
-            <li>Message: {req.body.message}</li>`
+            <li>Name: ${req.body.name}</li>
+            <li>Email: ${req.body.email}</li>
+            <li>Password: ${req.body.password}</li>
+            <li>Message: ${req.body.message}</li>`
   };
   transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
+      console.log(`Trying to send: \n ${mailOptions.html} \n but following error occur \n`);
       console.log(error);
       res.redirect('/');
     }else {
